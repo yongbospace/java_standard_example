a150a72a9f872ef16b2d016fa063128f81da9e88
yongbo <yongboshim@gmail.com>
2023-02-06 15:48:19 +0900

    23/02/06 to operator

---
+++ src/ch03/ex28.java
@@ -0,0 +1,23 @@
+package ch03;
+
+public class ex28 {
+    public static void main(String[] args) {
+        int x = 0xAB, y = 0xF;
+
+        System.out.printf("x = %#X \t\t\t\t\t%s\n", x, toBinaryString(x));
+        System.out.printf("y = %#X \t\t\t\t\t%s\n", y, toBinaryString(y));
+        System.out.printf("%#X | %#X = %#X \t\t\t%s%n", x, y, x | y, toBinaryString(x | y));
+
+        System.out.printf("%#X & %#X = %#X \t\t\t%s%n", x, y, x & y, toBinaryString(x & y));
+
+        System.out.printf("%#X ^ %#X = %#X \t\t\t%s%n", x, y, x ^ y, toBinaryString(x ^ y));
+
+        System.out.printf("%#X ^ %#X ^ %#X = %#X \t%s%n", x, y, y, x ^ y ^ y, toBinaryString(x ^ y ^ y));
+    }
+
+    static String toBinaryString(int x) {
+        String zero = "00000000000000000000000000000000";
+        String tmp = zero + Integer.toBinaryString(x);
+        return tmp.substring(tmp.length() - 32);
+    }
+}
